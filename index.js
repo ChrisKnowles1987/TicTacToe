ed128bceea4db8ac24bc6a5e5ff11b25d8de03df
Chris Knowles <chrisknowles1987@me.com>
2023-10-19 23:43:26 +0100

    createArray and checkWinner functions

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
 var marker = "X"
 
-
-
-let turns = 0
 function cellClicked(event){
-    
     event.target.textContent= marker
-    marker = marker === "X"? 'O': 'X'
     
     checkWinner()
-
+    marker = marker === "X"? 'O': 'X'
 }
 
-// grid = document.getElementsByClassName('cell')
-// firstCell= grid[0]
-    
-
-
-function checkWinner(){
+function makeArray(){
     grid = document.getElementsByClassName('cell')
-    XOArray =[]
-    const connect3 = [
+    let XOArray = []
+    for ( index = 0; index < grid.length; index++)
+    {
+    currentCell = grid[index]
+    XOArray.push(currentCell.innerText)
+    }  
+    return XOArray;
+}
+function checkWinner(){
+    XOArray=makeArray()
+    
+    connect3 = [
         [0,1,2],
         [3,4,5],
-        
-        ]
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    //point to the index of each cell for each winning line
+    for (let i = 0; i < connect3.length; i++) {
        
-    
-    
-    for ( index = 0; index < grid.length; index++) {
-        currentCell = grid[index]
+        const connect3Line = connect3[i]
         
-        if (currentCell.innerText === 'X'){
-            XOArray.push('X')
-            }
-        if (currentCell.innerText === 'O'){
-            XOArray.push('X')
-            }
-        else {
-            XOArray.push('')
-        }
-        }
-        XOArray.forEach((value, index) => {
-            console.log(value,index,)
+        let a = XOArray[connect3Line[0]]
+        let b = XOArray[connect3Line[1]]
+        let c = XOArray[connect3Line[2]]
+        console.log(a,  b , c)
+        // check if each of the indexes are the same (X or O) in each winning line
+        
+        if (a === b && b === c && a === 'X' || a ==='O') {
+            Winner = true
+            console.log(marker + ' wins!')
             
-        });
-                
-            
+            return
+       
         }
-    
-    
+    }
+}
+
 
